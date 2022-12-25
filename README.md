68c76dfebb5f1464196d8b62019c252c7a88b2ed
AliceLeyou <Liia.Alice.Menke@gmail.com>
2022-12-25 16:04:41 +0100

    Updated .gitignore:  - exclude Intermediate folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# unRealize
-unRealize is a simple ORM for Unreal Engine 5 that uses Unreal Engines reflection system to map structs to relational database objects. 
--- Source/UnRealize/Test/InitializeSampleModel.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE public."SampleEntityWithAllTypes" (
-    "Id" VARCHAR(255) NOT NULL,
-    "SampleBoolean" BOOLEAN,
-    "SampleSmallInt" SMALLINT,
-    "SampleInteger" INTEGER,
-    "SampleLong" BIGINT,
-    "SampleFloat" FLOAT4,
-    "SampleDouble" FLOAT8,
-    CONSTRAINT "TestEntityPK" PRIMARY KEY ("Id"));
